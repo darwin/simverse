dfa6aee539f4855c726ed04367f88a38c4f43bb5
Antonin Hildebrand <antonin@hildebrand.cz>
2019-04-25 01:22:17 +0200

    implement poor man's revisioning system for aux images

--- recipes/cookbook/scaffold/helpers/lib/utils.sh
+++ recipes/cookbook/scaffold/helpers/lib/utils.sh
@@ -15,3 +15,13 @@ echo_err() {
 docker_image_exists() {
   test "$(docker images -q "$1" 2> /dev/null)" != ""
 }
+
+docker_image_rev() {
+  local image_name=${1:?required}
+  local rev="$(docker inspect --format "{{index .Config.Labels \"simverse.rev\" }}" "${image_name}")"
+  if [[ -z "$rev" ]]; then
+    echo -n "0";
+  else
+    echo -n "$rev"
+  fi
+}
