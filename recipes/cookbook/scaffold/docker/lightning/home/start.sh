07914a22aeb3db779f6715259035fde487c44277
Antonin Hildebrand <antonin@hildebrand.cz>
2019-05-01 00:55:37 +0200

    waiting for bitcoin node open RPC socket is not good enough

--- recipes/cookbook/scaffold/docker/lightning/home/start.sh
+++ recipes/cookbook/scaffold/docker/lightning/home/start.sh
@@ -7,4 +7,9 @@ LIGHTNING_BITCOIN_RPC_HOST=${LIGHTNING_BITCOIN_RPC_HOST:?required}
 BITCOIN_RPC_PORT=${BITCOIN_RPC_PORT:?required}
 wait_for_socket "$BITCOIN_RPC_PORT" "$LIGHTNING_BITCOIN_RPC_HOST"
 
+# Agh, socket availability still does not necessary mean node readiness
+# RPC calls could fail with -28: Loading wallet or other errors
+# TODO: find a better solution
+sleep 10
+
 exec lightningd.sh
--- recipes/cookbook/scaffold/docker/lnd/home/start.sh
+++ recipes/cookbook/scaffold/docker/lnd/home/start.sh
@@ -10,4 +10,9 @@ LND_BITCOIN_RPC_HOST=${LND_BITCOIN_RPC_HOST:?required}
 BITCOIN_RPC_PORT=${BITCOIN_RPC_PORT:?required}
 wait_for_socket "$BITCOIN_RPC_PORT" "$LND_BITCOIN_RPC_HOST"
 
+# Agh, socket availability still does not necessary mean node readiness
+# RPC calls could fail with -28: Loading wallet or other errors
+# TODO: find a better solution
+sleep 10
+
 exec lnd.sh
