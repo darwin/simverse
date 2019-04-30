f373d5765fa9d45f28a0f7446eae64c862fc1a1c
Antonin Hildebrand <antonin@hildebrand.cz>
2019-04-30 16:04:12 +0200

    add more recipe permutations

---
+++ recipes/a1k1a1l2.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+. cookbook/cookbook.sh
+
+prelude
+
+add bitcoind b1
+add lightningd alice
+
+add bitcoind b2
+add lnd bob
+add lnd charlie
+
+# generate init script to build connections
+cat > init <<EOF
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+# connect bitcoin nodes
+connect b1 b2
+
+# connect LN nodes
+connect alice charlie
+connect charlie bob
+EOF
+chmod +x init
+
+cat > fund_all <<EOF
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+fund alice 10
+fund charlie 5
+EOF
+chmod +x fund_all
+
+cat > open_channels <<EOF
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+open_channel alice charlie 0.1
+open_channel charlie bob 0.1
+EOF
+chmod +x open_channels
