df8d59ffc154e2a9cf5d4c9b21c7676ffddc5f10
Antonin Hildebrand <antonin@hildebrand.cz>
2019-04-30 23:15:49 +0200

    btcd: review setup-wallet.sh

--- recipes/cookbook/scaffold/docker/btcd/home/setup-wallet.sh
+++ recipes/cookbook/scaffold/docker/btcd/home/setup-wallet.sh
@@ -5,7 +5,6 @@ source lib/init.sh
 FAUCET_ADDR_PRIVATE_KEY=${FAUCET_ADDR_PRIVATE_KEY:?required}
 
 # we need to wait for btcwallet to connect to btcd to avoid "-1: Chain RPC is inactive" errors
-set +e
 PROBE_CMD="./btcctl.sh --wallet getbalance"
 PROBE_COUNTER=1
 MAX_PROBES=100
@@ -17,11 +16,9 @@ while ! ${PROBE_CMD} > /dev/null 2>&1; do
     exit 1 # this will stop whole container with failure and bring it to user attention
   fi
 done
-set -e
 
 echo "Initializing wallet..."
 
 set -x
-
 ./btcctl.sh --wallet walletpassphrase "password" 0
 ./btcctl.sh --wallet importprivkey ${FAUCET_ADDR_PRIVATE_KEY} imported
