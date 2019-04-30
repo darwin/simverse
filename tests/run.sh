7bbe9701fbc3d5eade0a2e29d23ddb3c114ccdd1
Antonin Hildebrand <antonin@hildebrand.cz>
2019-04-30 16:04:20 +0200

    add tests

--- tests/run.sh
+++ tests/run.sh
@@ -69,6 +69,10 @@ announce "running tests from '$(pwd -P)'"
 ./test-cases/02-pay-via-charlie/test.sh a1k2l1
 ./test-cases/02-pay-via-charlie/test.sh a1l2k1
 
+./test-cases/02-pay-via-charlie/test.sh a1l1a1l2
+./test-cases/02-pay-via-charlie/test.sh a1k1a1k2
+./test-cases/02-pay-via-charlie/test.sh a1k1a1l2
+
 # this one is broken with "Incoming htlc(<id>) has an expiration that is too soon" for now
 #./test-cases/02-pay-via-charlie/test.sh a1k1b1l2
 
