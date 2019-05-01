e94d3c030c724c9f3c3136e13fc0b3b82cde0292
Antonin Hildebrand <antonin@hildebrand.cz>
2019-05-01 20:21:16 +0200

    let ln nodes wait for bitcoin node readiness

--- recipes/cookbook/scaffold/docker/lightning/home/start.sh
+++ recipes/cookbook/scaffold/docker/lightning/home/start.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-. lib/utils.sh
+source lib/init.sh
+source lib/utils.sh
 
 # wait for bitcoin node to open its RPC interface
 LIGHTNING_BITCOIN_RPC_HOST=${LIGHTNING_BITCOIN_RPC_HOST:?required}
-BITCOIN_RPC_PORT=${BITCOIN_RPC_PORT:?required}
-wait_for_socket "$BITCOIN_RPC_PORT" "$LIGHTNING_BITCOIN_RPC_HOST"
-
-# Agh, socket availability still does not necessary mean node readiness
-# RPC calls could fail with -28: Loading wallet or other errors
-# TODO: find a better solution
-sleep 10
+SERVICE_READY_PORT=${SERVICE_READY_PORT:?required}
+echo "Waiting for bitcoin node $LIGHTNING_BITCOIN_RPC_HOST to get ready..."
+wait_for_socket "$SERVICE_READY_PORT" "$LIGHTNING_BITCOIN_RPC_HOST"
 
 exec lightningd.sh
--- recipes/cookbook/scaffold/docker/lnd/home/start.sh
+++ recipes/cookbook/scaffold/docker/lnd/home/start.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-. lib/utils.sh
+source lib/init.sh
+source lib/utils.sh
 
 # wait for shared certificate creation
 while [[ ! -f /certs/rpc.cert ]]; do sleep 1; done
 
 # wait for bitcoin node to open its RPC interface
+echo "Waiting for bitcoin node $LIGHTNING_BITCOIN_RPC_HOST to get ready..."
 LND_BITCOIN_RPC_HOST=${LND_BITCOIN_RPC_HOST:?required}
-BITCOIN_RPC_PORT=${BITCOIN_RPC_PORT:?required}
-wait_for_socket "$BITCOIN_RPC_PORT" "$LND_BITCOIN_RPC_HOST"
-
-# Agh, socket availability still does not necessary mean node readiness
-# RPC calls could fail with -28: Loading wallet or other errors
-# TODO: find a better solution
-sleep 10
+SERVICE_READY_PORT=${SERVICE_READY_PORT:?required}
+wait_for_socket "$SERVICE_READY_PORT" "$LND_BITCOIN_RPC_HOST"
 
 exec lnd.sh
