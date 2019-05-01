1bebfb19a8fe7748ca3574bd5be4362e65fd7b31
Antonin Hildebrand <antonin@hildebrand.cz>
2019-05-01 21:43:53 +0200

    lnd: fix bitcoin node waiting message

--- recipes/cookbook/scaffold/docker/lnd/home/start.sh
+++ recipes/cookbook/scaffold/docker/lnd/home/start.sh
@@ -7,9 +7,9 @@ source lib/utils.sh
 while [[ ! -f /certs/rpc.cert ]]; do sleep 1; done
 
 # wait for bitcoin node to open its RPC interface
-echo "Waiting for bitcoin node $LIGHTNING_BITCOIN_RPC_HOST to get ready..."
 LND_BITCOIN_RPC_HOST=${LND_BITCOIN_RPC_HOST:?required}
 SERVICE_READY_PORT=${SERVICE_READY_PORT:?required}
+echo "Waiting for bitcoin node $LND_BITCOIN_RPC_HOST to get ready..."
 wait_for_socket "$SERVICE_READY_PORT" "$LND_BITCOIN_RPC_HOST"
 
 exec lnd.sh
