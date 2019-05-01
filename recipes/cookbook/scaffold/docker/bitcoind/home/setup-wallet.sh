93deb8292c822313693b6711e46e843d09fc8e53
Antonin Hildebrand <antonin@hildebrand.cz>
2019-05-01 20:17:51 +0200

    bitcoind: poll for bitcoin-cli readiness before talking to it

--- recipes/cookbook/scaffold/docker/bitcoind/home/setup-wallet.sh
+++ recipes/cookbook/scaffold/docker/bitcoind/home/setup-wallet.sh
@@ -6,6 +6,18 @@ FAUCET_ADDR_PRIVATE_KEY=${FAUCET_ADDR_PRIVATE_KEY:?required}
 
 echo "Initializing wallet..."
 
-set -x
+# we need to wait for bitcoin-cli start responding
+PROBE_CMD="./bitcoin-cli.sh -getinfo"
+PROBE_COUNTER=1
+MAX_PROBES=100
+while ! ${PROBE_CMD} > /dev/null 2>&1; do
+  sleep 1
+  ((++PROBE_COUNTER))
+  if [[ ${PROBE_COUNTER} -gt ${MAX_PROBES} ]]; then
+    echo "bitcoin-cli didn't come online in time"
+    exit 1 # this will stop whole container with failure and bring it to user attention
+  fi
+done
 
+set -x
 ./bitcoin-cli.sh importprivkey ${FAUCET_ADDR_PRIVATE_KEY} imported
--- recipes/cookbook/scaffold/docker/bitcoind/home/setup.sh
+++ recipes/cookbook/scaffold/docker/bitcoind/home/setup.sh
@@ -2,9 +2,6 @@
 
 source lib/init.sh
 
-# TODO: replace this with polling
-sleep 10
-
 ./setup-wallet.sh
 
 ./setup-chain.sh
