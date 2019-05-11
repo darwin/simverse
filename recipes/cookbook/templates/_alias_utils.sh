49d4baf6fce7a8826e1352438e786669c16542a7
Antonin Hildebrand <antonin@hildebrand.cz>
2019-05-11 23:39:10 +0200

    fix beautify_if_needed for cases when input does not have a new line

--- recipes/cookbook/templates/_alias_utils.sh
+++ recipes/cookbook/templates/_alias_utils.sh
@@ -7,7 +7,7 @@ SIMVERSE_DONT_BEAUTIFY_ALIASES=${SIMVERSE_DONT_BEAUTIFY_ALIASES}
 
 beautify_if_needed() {
   local first_line
-  read -r first_line
+  read -r first_line || [[ -n "$first_line" ]]
   if [[ -n "$SIMVERSE_DONT_BEAUTIFY_ALIASES" || "$first_line" != "{"* ]]; then
     echo "$first_line"; cat -
   else
