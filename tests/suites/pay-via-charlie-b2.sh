473b8e45add5a8eb0ccf7415e7f8b66c29c83cb4
Antonin Hildebrand <antonin@hildebrand.cz>
2019-09-20 21:30:16 +0200

    tests: disable eclair-involved tests for now
    until https://github.com/ACINQ/eclair/issues/1144

--- tests/suites/pay-via-charlie-b2.sh
+++ tests/suites/pay-via-charlie-b2.sh
@@ -4,4 +4,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 ./../cases/02-pay-via-charlie/test.sh a1k1a1k2
 ./../cases/02-pay-via-charlie/test.sh a1l1a1l2
-./../cases/02-pay-via-charlie/test.sh a1m1a1m2
+#./../cases/02-pay-via-charlie/test.sh a1m1a1m2
--- tests/suites/pay-via-charlie.sh
+++ tests/suites/pay-via-charlie.sh
@@ -4,4 +4,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 ./../cases/02-pay-via-charlie/test.sh b1l3
 ./../cases/02-pay-via-charlie/test.sh a1k3
-./../cases/02-pay-via-charlie/test.sh a1m3
+#./../cases/02-pay-via-charlie/test.sh a1m3
