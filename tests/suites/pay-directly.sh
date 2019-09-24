63e5d1ce08dfe2478788207377ddb1275dde31ba
Antonin Hildebrand <antonin@hildebrand.cz>
2019-09-24 12:19:57 +0200

    tests: enable eclair-involved tests again
    Revert "tests: disable eclair-involved tests for now"
    
    473b8e45add5a8eb0ccf7415e7f8b66c29c83cb4
    
    see https://github.com/ACINQ/eclair/issues/1144

--- tests/suites/pay-directly.sh
+++ tests/suites/pay-directly.sh
@@ -4,5 +4,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 ./../cases/01-pay-directly/test.sh a1k2
 ./../cases/01-pay-directly/test.sh b1l2
-#./../cases/01-pay-directly/test.sh a1m2
+./../cases/01-pay-directly/test.sh a1m2
 ./../cases/01-pay-directly/test.sh a1l2 # https://github.com/darwin/simverse/issues/7
